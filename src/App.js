78a619b07d6edf373cb6e884a261f3419c916091
mishanakp <mishanakp.official@gmail.com>
2023-09-25 17:56:35 +0530

    add more components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
+import Works from "./components/Works/works";
+import Contact from "./components/Contact/contact";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Navbar/>
       <Intro/>
       <Skills/>
+      <Works/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/Contact/contact.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './contact.css';
+
+const Contact = () => {
+  return (
+    <section id="contactPage">
+         <div className="projects">
+             <h1 className="projectsTitle">My Projects</h1>
+             <p className="projectsDesc">dshdjfkgkgkfhdydtdfjjgjhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhfffffffffffffffffffffffffffffffffffffffsdd</p>
+             <div className="projectBars">
+                  <div className="projectBar">
+                     <h1 className="projectDesc">career consultancy</h1>
+                  </div>
+                  <div className="projectBar">
+                     <h1 className="projectDesc">Movie app</h1>
+                  </div>
+                  <div className="projectBar">
+                     <h1 className="projectDesc">signup page</h1>
+                  </div>
+                  <div className="projectBar">
+                     <h1 className="projectDesc">portfolio </h1>
+                  </div>
+             </div>
+         </div>
+         <div className="contact">
+
+         </div>
+    </section>
+  )
+}
+
+export default Contact
